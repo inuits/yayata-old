b906cb2875185cb52f8a52dde01f25bfd2485855
Julien Pivotto <roidelapluie@inuits.eu>
2015-02-06 11:30:57 +0100

    allow admin of projects

--- backend/yabe/yata/admin.py
+++ backend/yabe/yata/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from models import Customer
+from models import Customer, Project
 
 admin.site.register(Customer, admin.ModelAdmin)
+admin.site.register(Project, admin.ModelAdmin)
 
