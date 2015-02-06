479a071b57b84a09ff114e1be21ecff181a42d2a
Julien Pivotto <roidelapluie@inuits.eu>
2015-02-06 11:15:33 +0100

    Add Projects model

--- backend/yabe/yata/models.py
+++ backend/yabe/yata/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django_countries.fields import CountryField
+from django.contrib.auth.models import Group
 
 class Customer(models.Model):
     short_name = models.CharField(max_length=5)
     name = models.CharField(max_length=30)
     country = CountryField()
+
+class Project(models.Model):
+    short_name = models.CharField(max_length=5)
+    name = models.CharField(max_length=30)
+    customer = models.ForeignKey('Customer')
+    start_date = models.DateField(blank=True)
+    end_date = models.DateField(blank=True)
+    billable = models.BooleanField(default=True)
+    groups = models.ManyToManyField(Group)
