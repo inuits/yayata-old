29a43f081b686dbc5908c934f11e984de332879e
Julien Pivotto <roidelapluie@inuits.eu>
2015-02-07 18:10:27 +0100

    Less stuff to begin with

--- beta/yata/models.py
+++ beta/yata/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django_countries.fields import CountryField
-from django.contrib.auth.models import Group
+#from django.contrib.auth.models import Group
 
 class Customer(models.Model):
     short_name = models.CharField(max_length=5)
@@ -14,10 +14,10 @@ class Project(models.Model):
     short_name = models.CharField(max_length=5)
     name = models.CharField(max_length=30)
     customer = models.ForeignKey('Customer')
-    start_date = models.DateField(blank=True,null=True)
-    end_date = models.DateField(blank=True,null=True)
-    billable = models.BooleanField(default=True)
-    groups = models.ManyToManyField(Group, blank=True)
+    #start_date = models.DateField(blank=True,null=True)
+    #end_date = models.DateField(blank=True,null=True)
+    #billable = models.BooleanField(default=True)
+    #groups = models.ManyToManyField(Group, blank=True)
 
     def __unicode__(self):
         return self.name
