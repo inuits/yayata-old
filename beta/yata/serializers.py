9204a8fc7a35afef4bb9a28920122b21292efff8
Julien Pivotto <roidelapluie@inuits.eu>
2015-02-08 16:23:30 +0100

    change api to a pk based one

--- beta/yata/serializers.py
+++ beta/yata/serializers.py
@@ -3,39 +3,39 @@ from rest_framework import serializers
 from models import Customer, Project, Timesheet, Hour, Company
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'groups')
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
+class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ('url', 'name')
 
 
-class CustomerSerializer(serializers.HyperlinkedModelSerializer):
+class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ('short_name', 'name', 'country')
 
-class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = ('short_name', 'name', 'customer')
 
-class TimesheetSerializer(serializers.HyperlinkedModelSerializer):
+class TimesheetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Timesheet
         fields = ('month','project','company','group','user')
 
-class HourSerializer(serializers.HyperlinkedModelSerializer):
+class HourSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hour
         fields = ('timesheet', 'description', 'hours', 'day')
 
-class CompanySerializer(serializers.HyperlinkedModelSerializer):
+class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
         fields = ('name',)
