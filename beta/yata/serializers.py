2d6bccb82779f3fc6eeeb103406658e0c7009b8e
Julien Pivotto <roidelapluie@inuits.eu>
2015-02-09 21:25:53 +0100

    add serializer

--- beta/yata/serializers.py
+++ beta/yata/serializers.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from models import Customer, Project, Timesheet, Hour, Company
 
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -26,6 +25,16 @@ class ProjectSerializer(serializers.ModelSerializer):
         fields = ('short_name', 'name', 'customer')
 
 class TimesheetSerializer(serializers.ModelSerializer):
+    def is_valid(self, *args, **kwargs):
+        is_valid = super(TimesheetSerializer, self).is_valid(*args, **kwargs)
+        validated_data = dict(
+            list(self.validated_data.items()) +
+            list(kwargs.items())
+        )
+        if validated_data['user'] == validated_data['group'] == None:
+            raise(serializers.ValidationError('You must set a user or a group'))
+        return is_valid
+
     class Meta:
         model = Timesheet
         fields = ('month','project','company','group','user')
