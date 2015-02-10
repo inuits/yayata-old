4b67af528152eb784dea77c4414ac3bb9ba4ae28
Julien Pivotto <roidelapluie@inuits.eu>
2015-02-10 22:03:19 +0100

    add ID and nice names

--- beta/yata/serializers.py
+++ beta/yata/serializers.py
@@ -5,26 +5,42 @@ from models import Customer, Project, Timesheet, Hour, Company
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
+        fields = ('id', 'url', 'username', 'email', 'groups')
 
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ('url', 'name')
+        fields = ('id', 'url', 'name')
 
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ('short_name', 'name', 'country')
+        fields = ('id', 'short_name', 'name', 'country')
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = ('short_name', 'name', 'customer')
+        fields = ('id', 'short_name', 'name', 'customer')
 
 class TimesheetSerializer(serializers.ModelSerializer):
+    project_name = serializers.StringRelatedField(source='project')
+    user_name = serializers.StringRelatedField(source='user')
+    company_name = serializers.StringRelatedField(source='company')
+    group_name = serializers.StringRelatedField(source='group')
+    customer_name = serializers.StringRelatedField(source='project.customer')
+
+
+    class Meta:
+        model = Timesheet
+        fields = ('id',
+                'month',
+                'project', 'project_name', 'customer_name',
+                'company', 'company_name',
+                'group', 'group_name',
+                'user', 'user_name')
+
     def is_valid(self, *args, **kwargs):
         is_valid = super(TimesheetSerializer, self).is_valid(*args, **kwargs)
         validated_data = dict(
@@ -35,17 +51,13 @@ class TimesheetSerializer(serializers.ModelSerializer):
             raise(serializers.ValidationError('You must set a user or a group'))
         return is_valid
 
-    class Meta:
-        model = Timesheet
-        fields = ('month','project','company','group','user')
-
 class HourSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hour
-        fields = ('timesheet', 'description', 'hours', 'day')
+        fields = ('id', 'timesheet', 'description', 'hours', 'day')
 
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
-        fields = ('name',)
+        fields = ('id', 'name',)
 
