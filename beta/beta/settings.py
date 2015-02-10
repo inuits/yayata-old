4b96351bbf796ca1adbfddcef7fd0f152884c940
Julien Pivotto <roidelapluie@inuits.eu>
2015-02-10 22:03:14 +0100

    Apply cors

--- beta/beta/settings.py
+++ beta/beta/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django_countries',
     'rest_framework',
+    'corsheaders',
     'yata',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -54,6 +56,8 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'beta.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 WSGI_APPLICATION = 'beta.wsgi.application'
 
